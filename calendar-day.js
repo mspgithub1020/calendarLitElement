66ac4551b2881d4c40af61f06fcadf1ebf67e2bd
mspgithub1020 <mspformacion1020@gmail.com>
2020-04-21 13:57:17 +0200

    week-day

--- calendar-day.js
+++ calendar-day.js
@@ -1,25 +1,30 @@
 import { LitElement, html, css } from 'https://cdn.pika.dev/lit-element';
-import { dateService } from './date-service.js';
-import { DateFormatter } from './date-formatter.js';
+
 
 class XCalendarDay extends LitElement {
 
+    //En este método creamos las propiedades del nuestro elemento
     static get properties(){
 
         return {
+            //propiedad de tipo Object
             date: {type: Object}
         };
 
     }
 
     constructor(){
-
+        //Invocamos a constructor del super de nuestro padre .... LitElement
         super();
+        //Inicializamos la propiedad date con la fecha actual
         this.date = new Date;
 
     }    
     
-    render() {        
+    //Implementamos el método que mostrará lo que contiene mi elemento 'x-calendar-day'
+    render() { 
+
+        //Devolvemos un template string con el día del valor de nuestra propiedad date 
         return html`<div>${this.date.getDate()}</div>`;
     }
 }
---
+++ week-day.js
@@ -0,0 +1,24 @@
+import { LitElement, html, css } from 'https://cdn.pika.dev/lit-element';
+
+class XWeekDay extends LitElement {
+
+    //En este método creamos las propiedades del nuestro elemento
+    static get properties(){
+
+        return {
+            //Propiedad de tipo String ... se le podrá asignar una Letra del día de la semana
+            day: {type: String}
+        };
+
+    }   
+    render() {  
+        //Si la propiedad 'day'no tiene valor asignado se devuelve null y se sale           
+        if(!this.day) {
+            return null;
+        }
+        //En el caso que se le asigne un valor lo mostrará
+        return html`<div>${this.day}</div>`;
+    }
+}
+
+customElements.define('x-calendar-weekday', XWeekDay);
