b6f43b0b1adc3e6c7b57e676fb7cbe6207caa3ec
mspgithub1020 <mspformacion1020@gmail.com>
2020-04-22 13:29:45 +0200

    calendar-clock

--- calendar-clock.js
+++ calendar-clock.js
@@ -2,40 +2,26 @@ import { LitElement, html } from 'https://cdn.pika.dev/lit-element';
 import { dateService } from './date-service.js';
 import { DateFormatter } from './date-formatter.js';    
  
-
+//se crea la clase XcalendarClock con BaseElement
 class XCalendarClock extends LitElement {
-    static get styles(){
-        return css`
-        .x-clock {
-            margin: 0;
-            font-size: var(--x-font-small);
-        `}
+
     static get properties() {
         return {
             date: { type: Object }
         };
     }
 
-    get $text() {
-        if (!this._$text) {
-            this._$text = this.renderRoot.getElementById('text');
-        }
-        return this._$text;
-    }
-
-    get timeString() {
-        return DateFormatter.timeString(dateService.date);
-        
-    }
     constructor() {
         super();
-        this.date = new Date();
+        this.date = dataService.date;
         
     }
+    //cada segundo hace una callback para actualizar los segundos
     connectedCallback() {
         super.connectedCallback();
         dateService.on(dateService.SECOND_CHANGED, this._onSecondChanged);
     }
+    //salta cuando se elimina el componente
     disconnectedCallback() {
         super.disconnectedCallback();
         dateService.off(dateService.SECOND_CHANGED, this._onSecondChanged);
